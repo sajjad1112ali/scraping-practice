2f36ed0279f4b47376854aa1bc7f85f329195780
Sajjad Ali <sajjad.ali@arpatech.com>
2023-03-27 13:34:11 +0500

    Login complete

--- index.js
+++ index.js
@@ -2,68 +2,61 @@ const request = require("request-promise");
 const cheerio = require("cheerio");
 const fs = require("fs/promises");
 
-const URL = "http://books.toscrape.com";
-const writeCSV = async (data, categoryName) => {
-  const csvData = data
-    .map(function (d) {
-      return d.join(",");
-    })
-    .join("\r\n");
-  console.log(`Creating ${categoryName}.csv file with ${data.length} records`);
-  await fs.writeFile(`./data/${categoryName}.csv`, csvData);
+const URL = "http://quotes.toscrape.com/login";
+const getCookies = (req) => {
+  return req.headers["set-cookie"].map((c) => c.split(";")[0]).join(" ");
 };
-const extractCategory = async (url, data) => {
-  const catResponse = await request(url);
-  let categoryPageCheerio = cheerio.load(catResponse);
-  const isPaginationAvailable = categoryPageCheerio("li.next > a");
-  
-  console.log(isPaginationAvailable.length);
-
-  const re = new RegExp(",", "g");
-
-  let items = categoryPageCheerio("ol.row > li");
-  const itemsLength = items.length;
-  //extractItemData();
-  for (let index = 0; index < itemsLength; index++) {
-    const itemData = [];
-    const item = items[index];
-    const image = categoryPageCheerio(item)
-      .find("div.image_container > a > img")
-      .attr("src");
-    const title = categoryPageCheerio(item)
-      .find("h3 > a")
-      .attr("title")
-      .replace(re, "~");
-    const price = categoryPageCheerio(item)
-      .find(".product_price > p.price_color")
-      .text();
-    itemData.push(image);
-    itemData.push(title);
-    itemData.push(price);
-    data.push(itemData);
-  }
-
-  if (isPaginationAvailable.length){
-    const paginationURL = isPaginationAvailable.attr("href");
-    const arrOfURL =  url.split("/");
-    const removedLastItem = arrOfURL.slice(0, -1);
-    const baseURL = removedLastItem.join("/");
-    await extractCategory(`${baseURL}/${paginationURL}`, data)
-    }
-    return data;
+const getBasicHeaders = () => {
+  return {
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "max-age=0",
+    Connection: "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded",
+    Host: "quotes.toscrape.com",
+    Origin: "http://quotes.toscrape.com",
+    Referer: "http://quotes.toscrape.com/login",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent":
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+  };
 };
+
 (async () => {
-  const response = await request(URL);
-  let mainCheerio = cheerio.load(response);
-  const navList = mainCheerio("ul.nav.nav-list > li > ul > li");
-  const myTargetArr = [navList[3]];
-  const newArr = myTargetArr.splice(0, 1);
-  for (nav of navList) {
-    const url = mainCheerio(nav).find("a").attr("href");
-    const categoryName = mainCheerio(nav).find("a").text().trim();
-    console.log(`categoryName = ${categoryName}`);
-    const data = [["Image URL", "Title", "Price"]];
-    await extractCategory(`${URL}/${url}`, data);
-    await writeCSV(data, categoryName);
+  try {
+    const initialRequest = await request({
+      uri: URL,
+      method: "GET",
+      resolveWithFullResponse: true,
+    });
+    let cookies = getCookies(initialRequest);
+    let headers = getBasicHeaders();
+    headers["Cookie"] = cookies;
+    let $ = cheerio.load(initialRequest.body);
+    let csrfToken = $('input[name="csrf_token"]').val();
+
+    const loginRequest = await request({
+      uri: URL,
+      method: "POST",
+      headers,
+      form: {
+        csrf_token: csrfToken,
+        username: "admin",
+        password: "admin",
+      },
+      resolveWithFullResponse: true,
+    });
+  } catch (response) {
+    cookies = getCookies(response.response);
+    let headers = getBasicHeaders();
+    headers["Cookie"] = cookies;
+    delete headers["Content-Type"];
+    const loggedInRequest = await request({
+      uri: "http://quotes.toscrape.com/",
+      method: "GET",
+      headers,
+    });
   }
 })();
