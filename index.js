4a1a39381d26bae2319c33ce3fe6bbd04f8e990f
sajjad1112ali <sajjadramzan1211@gmail.com>
2023-04-07 10:40:04 +0500

    Extract all quotes

--- index.js
+++ index.js
@@ -3,10 +3,55 @@ const { Parser } = require("json2csv");
 const fs = require("fs/promises");
 
 const getTextArray = async (page, selector) => {
-  const quotesArray = await page.$$eval(selector, (quote) =>
-    quote.map((g) => ({ name: g.innerText }))
+  const selectorArray = await page.$$eval(selector, (s) =>
+    s.map((g) => ({ value: g.innerText }))
   );
-  return quotesArray;
+  return selectorArray;
+};
+
+const getTagArray = async (page, selector) => {
+  const els = await page.$$(selector);
+  let tagsOfPost = [];
+  for (let i = 0; i < els.length; i++) {
+    const link = await els[i].$$eval("a", (a) => a.map((b) => b.innerText));
+    tagsOfPost.push(link);
+  }
+  return tagsOfPost;
+};
+let sNo = 0;
+const getQuotesObject = (quotes, writers, tags, data) => {
+  quotes.map((quote, index) => {
+    
+    data.push({
+      newKey: ++sNo,
+      quote: quote.value,
+      writer: writers[index].value,
+      tags: tags[index].join(","),
+    });
+  });
+  return data;
+};
+
+const getQuotesData = async (page, data) => {
+  const quotesArray = await getTextArray(page, "div.quote > span.text");
+  const writersArray = await getTextArray(
+    page,
+    "div.quote > span > small.author"
+  );
+  const tagsOfPost = await getTagArray(page, "div.quote > div.tags");
+  const nextBtn = await page.evaluate(() => {
+    const element = document.querySelector("ul.pager > li.next > a");
+    if (element) {
+      return true;
+    }
+    return false;
+  });
+  getQuotesObject(quotesArray, writersArray, tagsOfPost, data);
+  if (nextBtn) {
+    await page.click("ul.pager > li.next > a");
+    await getQuotesData(page, data);
+  }
+  return data;
 };
 
 (async () => {
@@ -17,28 +62,17 @@ const getTextArray = async (page, selector) => {
       userDataDir: "./temp",
     });
 
-    let isNextBtnAvailable = true;
+    let csvData = [];
     const page = await browser.newPage();
-    debugger;
 
     await page.goto("http://quotes.toscrape.com/js/");
 
-    const quotesArray = await getTextArray(page, "div.quote > span.text");
-    const writersArray = await getTextArray(
-      page,
-      "div.quote > span > small.author"
-    );
-    const tagsArray = await getTextArray(page, "div.quote > div.tags");
+    await getQuotesData(page, csvData);
 
-    const nextBtn = await page.evaluate(() => {
-      const element = document.querySelector("ul.pager > li.next > a");
-      if (element) {
-        return true;
-      }
-      return false;
-    });
-    if (nextBtn) await page.click("ul.pager > li.next > a");
-    // await browser.close();
+    const parser = new Parser();
+    const csv = parser.parse(csvData);
+    await fs.writeFile(`./data/allQuotes.csv`, csv);
+    await browser.close();
   } catch (error) {
     console.log(error);
   }
